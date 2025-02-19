066aad87ed084d6d4501378f1323918741cdaf50
Vishvjeet-Rana <aryanrana62366@gmail.com>
2025-02-19 13:51:49 +0530

    vite config file changed

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/AetherCore-GamingAI-Site/",
+  build: {
+    outDir: "./docs", // More explicit relative path
+    sourcemap: true,
+  },
 });
